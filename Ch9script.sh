1bbd05b77edaf4671878d259d856e77c38e86a68
vangx033 <seeho.vang@hotmail.com>
2018-03-27 20:10:24 -0700

    Ch9script.sh

--- Ch9script.sh
+++ Ch9script.sh
@@ -5,4 +5,15 @@
 # Viewing infomation about your IP address
 ifconfig
 
+#Viewing routing table
+route
+
+# Using ping command and stop after 10 pings 
+ping -c 10 www.google.com
+
+# Using traceroute command
+traceroute -n www.google.com
+
+# DNS look up
+host www.google.com
 
